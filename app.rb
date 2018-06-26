6e4e45069854bf4098fcda1390830cc4e0693b13
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-26 18:36:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+      "My name is Fatoumata"
+    end
+  
+    get '/ hometown'do
+      "My hometown is owings mills"
+    end
+  
+    get '/ favorite-song'do
+      "My favorite song is that's my girl thats my girl"
+    end
 end
